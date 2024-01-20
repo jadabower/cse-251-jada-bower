08e949b8a84a86eb729ace8fa7eda270f52e84ec
Jada Bower <131042500+jadabower@users.noreply.github.com>
2024-01-20 16:06:57 -0700

    PROVE WORKS!!!

--- lesson_02/prove/prove.py
+++ lesson_02/prove/prove.py
@@ -83,15 +83,29 @@ class Request_Thread(threading.Thread):
         else:
             print('RESPONSE = ', response.status_code)
 
-def Get_One_Item(dict, index, url_so_far, threads, key):
-    url = f'{url_so_far}{dict[f"{index}"]}'
+def Get_One_Item(dict, index, threads, key):
+    url = f'{dict[index]}'
     thread = Request_Thread(url)
     thread.start()
     threads[key].append(thread)
 
+def Stringify_List(list):
+    result = ''
+    for item in list:
+      if item == list[-1]:
+        result += f' {item}'
+      elif item == list[0]:
+        result += f'{item},'
+      else:
+        result += f' {item},'
+    return result
+    
+
 def main():
+    global call_count
     log = Log(show_terminal=True)
     log.start_timer('Starting to retrieve data from the server')
+    log.write('----------------------------------------')
 
     threads = {
        'chars':[],
@@ -103,48 +117,58 @@ def main():
 
     # TOP DATA
     top_data_thread = Request_Thread(TOP_API_URL)
+    call_count += 1
     top_data_thread.start()
     top_data_thread.join()
     top_data = top_data_thread.response
 
     # REVENGE OF THE SITH DATA
-    rots_url = f'{TOP_API_URL}{top_data["films"]}6'
+    rots_url = f'{top_data["films"]}6'
     rots_data_thread = Request_Thread(rots_url)
+    call_count += 1
     rots_data_thread.start()
     rots_data_thread.join()
     rots_data = rots_data_thread.response
-    print('__________________________________________')
-    log.write(rots_data)
+
+    title = rots_data['title']
+    director = rots_data['director']
+    producer = rots_data['producer']
+    release_date = rots_data['release_date']
 
     # CHARACTER THREADS
     char_data = rots_data["characters"]
     num_of_chars = len(char_data)
     for i in range(num_of_chars):
-      Get_One_Item(char_data, i, rots_url, threads, 'chars')
+      Get_One_Item(char_data, i, threads, 'chars')
+      call_count += 1
         
     # PLANET THREADS
     planet_data = rots_data["planets"]
     num_of_planets = len(planet_data)
     for i in range(num_of_planets):
-      Get_One_Item(planet_data, i, rots_url, threads, 'planets')
+      Get_One_Item(planet_data, i, threads, 'planets')
+      call_count += 1
 
     # STARSHIP THREADS
     starship_data = rots_data["starships"]
     num_of_starships = len(starship_data)
     for i in range(num_of_starships):
-      Get_One_Item(starship_data, i, rots_url, threads, 'starships')
+      Get_One_Item(starship_data, i, threads, 'starships')
+      call_count += 1
 
     # VEHICLE THREADS
     vehicle_data = rots_data["vehicles"]
     num_of_vehicles = len(vehicle_data)
     for i in range(num_of_vehicles):
-      Get_One_Item(vehicle_data, i, rots_url, threads, 'vehicles')
+      Get_One_Item(vehicle_data, i, threads, 'vehicles')
+      call_count += 1
 
     # SPECIES THREADS
     species_data = rots_data["species"]
     num_of_species = len(species_data)
     for i in range(num_of_species):
-      Get_One_Item(species_data, i, rots_url, threads, 'species')
+      Get_One_Item(species_data, i, threads, 'species')
+      call_count += 1
 
     # JOIN THREADS AND SORT LISTS
     chars = []
@@ -178,20 +202,26 @@ def main():
     species.sort()
 
     # LOG EVERYTHING
+    log.write(f'Title   : {title}')
+    log.write(f'Director: {director}')
+    log.write(f'Producer: {producer}')
+    log.write(f'Released: {release_date}')
+    log.write()
     log.write(f'Characters: {num_of_chars}')
-    log.write(chars)
-
+    log.write(Stringify_List(chars))
+    log.write()
     log.write(f'Planets: {num_of_planets}')
-    log.write(planets)
-
+    log.write(Stringify_List(planets))
+    log.write()
     log.write(f'Starships: {num_of_starships}')
-    log.write(starships)
-
+    log.write(Stringify_List(starships))
+    log.write()
     log.write(f'Vehicles: {num_of_vehicles}')
-    log.write(vehicles)
-
+    log.write(Stringify_List(vehicles))
+    log.write()
     log.write(f'Species: {num_of_species}')
-    log.write(species)
+    log.write(Stringify_List(species))
+    log.write()
 
     # END LOG
     log.stop_timer('Total Time To complete')
