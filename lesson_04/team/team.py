f364c944a06237a16511d7baf56466f729515674
Jada Bower <131042500+jadabower@users.noreply.github.com>
2024-01-30 17:59:12 -0700

    team

--- lesson_04/team/team.py
+++ lesson_04/team/team.py
@@ -23,30 +23,47 @@ import json
 # Include cse 251 common Python files
 from cse251 import *
 
-RETRIEVE_THREADS = 4        # Number of retrieve_threads
+RETRIEVE_THREADS = 38        # Number of retrieve_threads
 NO_MORE_VALUES = 'No more'  # Special value to indicate no more items in the queue
 
-def retrieve_thread():  # TODO add arguments
+def retrieve_thread(queue, sem, log, i):
     """ Process values from the data_queue """
 
     while True:
-        # TODO check to see if anything is in the queue
+        # check to see if anything is in the queue
+        sem.acquire()
+        # process the value retrieved from the queue
+        url = queue.get()
+        if url == NO_MORE_VALUES:
+            break
+        else:
+            # make Internet call to get characters name and log it
+            response = requests.get(url)
+            # Check the status code to see if the request succeeded.
+            status_code = response.status_code
+            if response.status_code == 200:
+                response = response.json()
+                log.write(response['name'])
+            else:
+                print('RESPONSE = ', response.status_code)
 
-        # TODO process the value retrieved from the queue
 
-        # TODO make Internet call to get characters name and log it
-        pass
+def file_reader(queue, sem, log):
+    """ This thread reads the data file and places the values in the data_queue """
+    # Open the data file "urls.txt" and place items into a queue
+    with open("urls.txt") as urls:
+        for url in urls:
+            url = url.strip()
+            queue.put(url)
+            sem.release()
 
+    log.write(f'finished reading file with {RETRIEVE_THREADS} threads')
 
-
-def file_reader(): # TODO add arguments
-    """ This thread reading the data file and places the values in the data_queue """
-
-    # TODO Open the data file "urls.txt" and place items into a queue
-
-    log.write('finished reading file')
-
-    # TODO signal the retrieve threads one more time that there are "no more values"
+    # signal the retrieve threads one more time that there are "no more values"
+    for i in range(RETRIEVE_THREADS):
+        queue.put(NO_MORE_VALUES)
+        sem.release()
+    
 
 
 
@@ -55,17 +72,33 @@ def main():
 
     log = Log(show_terminal=True)
 
-    # TODO create queue
-    # TODO create semaphore (if needed)
+    # create queue
+    q = queue.Queue()
+    # create semaphore (if needed)
+    sem = threading.Semaphore(0)
 
-    # TODO create the threads. 1 filereader() and RETRIEVE_THREADS retrieve_thread()s
+    retrieve_threads = []
+
+    # create the threads. 1 filereader() and RETRIEVE_THREADS retrieve_thread()s
     # Pass any arguments to these thread need to do their job
+    file_reader_thread = threading.Thread(target=file_reader, args=(q, sem, log))
+
+    for i in range(RETRIEVE_THREADS):
+        retrieve_threads.append(threading.Thread(target=retrieve_thread, args=(q, sem, log, i)))
 
     log.start_timer()
 
-    # TODO Get them going - start the retrieve_threads first, then file_reader
+    # Get them going - start the retrieve_threads first, then file_reader
+    file_reader_thread.start()
 
-    # TODO Wait for them to finish - The order doesn't matter
+    for thread in retrieve_threads:
+        thread.start()
+
+    # Wait for them to finish - The order doesn't matter
+    file_reader_thread.join()
+
+    for thread in retrieve_threads:
+        thread.join()
 
     log.stop_timer('Time to process all URLS')
 
