363c912d7a952b3b553f7ddf70c6338334e2cd3d
Jada Bower <131042500+jadabower@users.noreply.github.com>
2024-03-26 22:16:07 -0600

    go assignments team

--- lesson_12/team1/team1.go
+++ lesson_12/team1/team1.go
@@ -40,89 +40,198 @@ Part 2
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"time"
+  "encoding/json"
+  "fmt"
+  "log"
+  "net/http"
+  "time"
+  "io"
+  "sync"
 )
 
 type Person struct {
-	Birth_year string
-	Created    time.Time
-	Edited     time.Time
-	Eye_color  string
-	Films      []string
-	Gender     string
-	Hair_color string
-	Height     string
-	Homeworld  string
-	Mass       string
-	Name       string
-	Skin_color string
-	Species    []string
-	Starships  []string
-	Url        string
-	Vehicles   []string
+  Birth_year string
+  Created    time.Time
+  Edited     time.Time
+  Eye_color  string
+  Films      []string
+  Gender     string
+  Hair_color string
+  Height     string
+  Homeworld  string
+  Mass       string
+  Name       string
+  Skin_color string
+  Species    []string
+  Starships  []string
+  Url        string
+  Vehicles   []string
 }
 
-func getPerson(url string) {
-	// make a sample HTTP GET request
-	res, err := http.Get(url)
-
-	// check for response error
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// read all response body
-	data, _ := ioutil.ReadAll(res.Body)
-
-	// close response body
-	res.Body.Close()
-
-	// fmt.Println(string(data))
-
-	person := Person{}
-	jsonErr := json.Unmarshal(data, &person)
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-		fmt.Println("ERROR Pasing the JSON")
-	}
-
-	fmt.Println("-----------------------------------------------")
-	// fmt.Println(person)
-	fmt.Println("Name      : ", person.Name)
-	fmt.Println("Birth     : ", person.Birth_year)
-	fmt.Println("Eye color : ", person.Eye_color)
+type Species struct {
+  Name             string
+  Created          time.Time
+  Edited           time.Time
+  Classification   string
+  Designation      string
+  Average_height   string
+  Skin_colors      string
+  Hair_colors      string
+  Eye_colors       string
+  Average_lifespan string
+  Homeworld        string
+  Language         string
+  People           []string
+  Films            []string
+  Url              string
 }
+
+func getPerson(url string, wg *sync.WaitGroup) {
+  // make a sample HTTP GET request
+  res, err := http.Get(url)
+
+  // check for response error
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  // Read all response body
+  data, readErr := io.ReadAll(res.Body)
+  if readErr != nil {
+    log.Fatal(readErr)
+  }
+
+  // close response body
+  res.Body.Close()
+
+  // fmt.Println(string(data))
+
+  person := Person{}
+  jsonErr := json.Unmarshal(data, &person)
+  if jsonErr != nil {
+    log.Fatal(jsonErr)
+    fmt.Println("ERROR Pasing the JSON")
+  }
+
+  fmt.Println("-----------------------------------------------")
+  // fmt.Println(person)
+  fmt.Println("Name      : ", person.Name)
+  fmt.Println("Birth     : ", person.Birth_year)
+  fmt.Println("Eye color : ", person.Eye_color)
+
+  wg.Done()
+}
+
+func getSpecies(url string, wg *sync.WaitGroup) {
+  // make a sample HTTP GET request
+  res, err := http.Get(url)
+
+  // check for response error
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  // Read all response body
+  data, readErr := io.ReadAll(res.Body)
+  if readErr != nil {
+    log.Fatal(readErr)
+  }
+
+  // close response body
+  res.Body.Close()
+
+  // fmt.Println(string(data))
+
+  species := Species{}
+  jsonErr := json.Unmarshal(data, &species)
+  if jsonErr != nil {
+    log.Fatal(jsonErr)
+    fmt.Println("ERROR Pasing the JSON")
+  }
+
+  // For a species, display name, average_height and language
+
+  fmt.Println("-----------------------------------------------")
+  // fmt.Println(person)
+  fmt.Println("Name      : ", species.Name)
+  fmt.Println("Height    : ", species.Average_height)
+  fmt.Println("Language  : ", species.Language)
+
+  wg.Done()
+}
+
 
 func main() {
-	urls := []string{
-		"http://swapi.dev/api/people/1/",
-		"http://swapi.dev/api/people/2/",
-		"http://swapi.dev/api/people/3/",
-		"http://swapi.dev/api/people/4/",
-		"http://swapi.dev/api/people/5/",
-		"http://swapi.dev/api/people/6/",
-		"http://swapi.dev/api/people/7/",
-		"http://swapi.dev/api/people/8/",
-		"http://swapi.dev/api/people/9/",
-		"http://swapi.dev/api/people/10/",
-		"http://swapi.dev/api/people/12/",
-		"http://swapi.dev/api/people/13/",
-		"http://swapi.dev/api/people/14/",
-		"http://swapi.dev/api/people/15/",
-		"http://swapi.dev/api/people/16/",
-		"http://swapi.dev/api/people/18/",
-		"http://swapi.dev/api/people/19/",
-		"http://swapi.dev/api/people/81/",
-	}
+  people_urls := []string{
+    "http://swapi.dev/api/people/1/",
+    "http://swapi.dev/api/people/2/",
+    "http://swapi.dev/api/people/3/",
+    "http://swapi.dev/api/people/4/",
+    "http://swapi.dev/api/people/5/",
+    "http://swapi.dev/api/people/6/",
+    "http://swapi.dev/api/people/7/",
+    "http://swapi.dev/api/people/8/",
+    "http://swapi.dev/api/people/9/",
+    "http://swapi.dev/api/people/10/",
+    "http://swapi.dev/api/people/12/",
+    "http://swapi.dev/api/people/13/",
+    "http://swapi.dev/api/people/14/",
+    "http://swapi.dev/api/people/15/",
+    "http://swapi.dev/api/people/16/",
+    "http://swapi.dev/api/people/18/",
+    "http://swapi.dev/api/people/19/",
+    "http://swapi.dev/api/people/81/",
+  }
 
-	for _, url := range urls {
-		getPerson(url)
-	}
+  fmt.Println("\nPEOPLE STARTED")
+  
+  var people_wg sync.WaitGroup
+  people_wg.Add(len(people_urls))
 
-	fmt.Println("All done!")
+  for _, url := range people_urls {
+    go getPerson(url, &people_wg)
+  }
+  
+  people_wg.Wait()
+
+  fmt.Println("\nPEOPLE DONE")
+
+  species_urls := []string{
+    "http://swapi.dev/api/species/1/",
+    "http://swapi.dev/api/species/2/",
+    "http://swapi.dev/api/species/3/",
+    "http://swapi.dev/api/species/6/",
+    "http://swapi.dev/api/species/15/",
+    "http://swapi.dev/api/species/19/",
+    "http://swapi.dev/api/species/20/",
+    "http://swapi.dev/api/species/23/",
+    "http://swapi.dev/api/species/24/",
+    "http://swapi.dev/api/species/25/",
+    "http://swapi.dev/api/species/26/",
+    "http://swapi.dev/api/species/27/",
+    "http://swapi.dev/api/species/28/",
+    "http://swapi.dev/api/species/29/",
+    "http://swapi.dev/api/species/30/",
+    "http://swapi.dev/api/species/33/",
+    "http://swapi.dev/api/species/34/",
+    "http://swapi.dev/api/species/35/",
+    "http://swapi.dev/api/species/36/",
+    "http://swapi.dev/api/species/37/",
+  }
+
+  fmt.Println("\nSPECIES STARTED")
+  
+  var species_wg sync.WaitGroup
+  species_wg.Add(len(species_urls))
+
+  for _, url := range species_urls {
+    go getSpecies(url, &species_wg)
+  }
+
+  species_wg.Wait()
+
+  fmt.Println("\nSPECIES DONE")
+  
+
+  fmt.Println("\nAll done!")
 }
--- lesson_12/team2/team2.go
+++ lesson_12/team2/team2.go
@@ -24,60 +24,94 @@ the prime numbers
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
+  "fmt"
+  "math/rand"
+  "time"
+  "sync"
 )
 
 func isPrime(n int) bool {
-	// Primality test using 6k+-1 optimization.
-	// From: https://en.wikipedia.org/wiki/Primality_test
+  // Primality test using 6k+-1 optimization.
+  // From: https://en.wikipedia.org/wiki/Primality_test
 
-	if n <= 3 {
-		return n > 1
-	}
+  if n <= 3 {
+    return n > 1
+  }
 
-	if n%2 == 0 || n%3 == 0 {
-		return false
-	}
+  if n%2 == 0 || n%3 == 0 {
+    return false
+  }
 
-	i := 5
-	for (i * i) <= n {
-		if n%i == 0 || n%(i+2) == 0 {
-			return false
-		}
-		i += 6
-	}
-	return true
+  i := 5
+  for (i * i) <= n {
+    if n%i == 0 || n%(i+2) == 0 {
+      return false
+    }
+    i += 6
+  }
+  return true
 }
 
-func worker() {
-	// TODO - process numbers on one channel and place prime number on another
+func worker(receive_from_ch chan int, send_to_ch chan int, wg *sync.WaitGroup, chunk_to_process int) {
+  // Process numbers on one channel and place prime number on another
+  for i := 0; i < chunk_to_process; i++ {
+    possible_prime := <- receive_from_ch
+    if isPrime(possible_prime) {
+      send_to_ch <- possible_prime
+    }
+  }
+  wg.Done()
 }
 
-func readValues() {
-	// TODO -Display prime numbers from a channel
+func readValues(send_to_ch chan int, wg *sync.WaitGroup) {
+  // Display prime numbers from a channel
+  valuesToGet := true
+  for valuesToGet {
+      prime := <- send_to_ch
+      if prime == -1 {
+          valuesToGet = false
+      } else {
+          fmt.Println("Prime: ", prime)
+      }
+  }
+  wg.Done()
 }
 
 func main() {
 
-	workers := 10
-	numberValues := 100
+  workers := 10
+  numberValues := 100
 
-	// Create any channels that you need
-	// Create any other "things" that you need to get the workers to finish(join)
+  // Create any channels that you need
+  receive_from_ch := make(chan int)
+  send_to_ch := make(chan int)
+  
+  // Create any other "things" that you need to get the workers to finish(join)
+  var wg sync.WaitGroup
+  wg.Add(workers + 1)
 
-	// create workers
-	for w := 1; w <= workers; w++ {
-		go worker() // Add any arguments
-	}
+  // create workers
+  chunk_per_worker := numberValues / workers
+  for w := 1; w <= workers; w++ {
+    go worker(receive_from_ch, send_to_ch, &wg, chunk_per_worker)
+  }
 
-	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < numberValues; i++ {
-		// ch <- rand.Int()
-	}
+  randSource := rand.NewSource(time.Now().UnixNano())
+  randGenerator := rand.New(randSource)
+  
+  go readValues(send_to_ch, &wg)
+  
+  // rand.Seed(time.Now().UnixNano())
+  for i := 0; i < numberValues; i++ {
+    receive_from_ch <- randGenerator.Int()
+  }
 
-	go readValues() // Add any arguments
+  send_to_ch <- -1
+  
+  wg.Wait()
+  
+  close(receive_from_ch)
+  close(send_to_ch)
 
-	fmt.Println("All Done!")
+  fmt.Println("All Done!")
 }
